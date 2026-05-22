91f968d48cb4736a6971de3b03b3c7e3d734b96a
Gluton <glutonofficial@gmail.com>
2026-05-22 02:07:03 -0400

    Implement Ascension 14

---
+++ GlutonsAscensions/Patches/EmptyFlasksPatches.cs
@@ -0,0 +1,30 @@
+using GlutonsAscensions.Helpers;
+using GlutonsAscensions.Models;
+using HarmonyLib;
+using MegaCrit.Sts2.Core.Models;
+using MegaCrit.Sts2.Core.Nodes.Potions;
+using MegaCrit.Sts2.Core.Rewards;
+
+namespace GlutonsAscensions.Patches;
+
+[HarmonyPatch]
+public class EmptyFlasksPatches {
+    private const float EmptyFlaskChance = 0.5f;
+
+    [HarmonyPatch(typeof(PotionReward), nameof(PotionReward.Populate))]
+    [HarmonyPostfix]
+    static void PopulatePostfix(PotionReward __instance) {
+        if (!GlutonsAscensionLevel.EmptyFlasks.HasAscension()) return;
+
+        var rng = __instance._rngOverride ?? __instance.Player.PlayerRng.Rewards;
+        if (rng.NextFloat() >= EmptyFlaskChance) {
+            __instance.Potion = ModelDb.Potion<EmptyPotion>().ToMutable();
+        }
+    }
+
+    [HarmonyPatch(typeof(NPotionPopup), nameof(NPotionPopup._Ready))]
+    [HarmonyPrefix]
+    static void _ReadyPrefix() {
+        
+    }
+}
