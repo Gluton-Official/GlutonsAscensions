dd53f2e58708b51e02747fd02ce125dbf731e9a2
Gluton <glutonofficial@gmail.com>
2026-05-22 23:52:19 -0400

    Implement Ascension 19

---
+++ GlutonsAscensions/Patches/UnpreparedPatches.cs
@@ -0,0 +1,19 @@
+using GlutonsAscensions.Helpers;
+using HarmonyLib;
+using MegaCrit.Sts2.Core.Combat;
+using MegaCrit.Sts2.Core.Hooks;
+
+namespace GlutonsAscensions.Patches;
+
+[HarmonyPatch]
+public class UnpreparedPatches {
+    [HarmonyPatch(typeof(Hook), nameof(Hook.ModifyHandDraw))]
+    [HarmonyPostfix]
+    static void ReduceStartOfCombatDrawCount(CombatState combatState, ref decimal __result) {
+        if (!GlutonsAscensionLevel.Unprepared.HasAscension()) return;
+
+        if (combatState.RoundNumber == 1) {
+            __result = Math.Max(__result - 1, 0);
+        }
+    }
+}
